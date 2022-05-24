2e6d186de8d0c9390e93e8122b4d4a94f0e13dbd
Alexander Schuessler <schuessler.alex@gmail.com>
2022-05-24 10:37:53 -0500

    commit tree eca51a1d2f73a42d9173d1c2bdde6273582338cf

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,10 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :dev
+    belongs_to :company
+    # join table between company and dev
+
+    def print_details
+        "#{self.dev[:name]} owns a #{self[:item_name]} from #{self.company.name}"
+    end
 
 end
